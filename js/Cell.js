b6a78c20f0fab7a395a5740d1647d66592d5110e
ArcziDudu <artur.augustyn26@gmail.com>
2022-06-13 21:14:00 +0200

    added bombs function

--- js/Cell.js
+++ js/Cell.js
@@ -5,7 +5,7 @@ export class Cell extends UI{
         this.x = x;
         this.y = y;
         this.value = 0;
-        this.isMine =false;
+        this.isMine = false;
         this.isReveal = false;
         this.isFlagged = false;
         this.selector = `[data-x="${this.x}"][data-y="${this.y}"]`;
@@ -23,5 +23,11 @@ export class Cell extends UI{
         this.isReveal = true;
         this.element.classList.remove('border--concave')
         this.element.classList.add('border--revealed')
+        if(this.isMine){
+          this.element.classList.add('cell--is-mine')
+        }
+      }
+      addMine(){
+        this.isMine = true;
       }
 }
